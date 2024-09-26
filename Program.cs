a2bef7e7f06faf6d93e79c126c9fbf3abd912aec
CHANDRA BOSE <CHANDRA BOSE@LAPTOP-0D044MST>
2024-09-26 22:52:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Palindromenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine($"{number} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a palindrome.");
+            }
+        }
+
+        static bool IsPalindrome(int num)
+        {
+            int originalNum = num;
+            int reversedNum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10; 
+                reversedNum = reversedNum * 10 + digit;
+                num /= 10; 
+            }
+
+            return originalNum == reversedNum;
+        }
+    }
+}
